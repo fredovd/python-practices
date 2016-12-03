03ffa123d230a37fa4541af1231ab3a5ec0395a7
fredovd <fred_ovd@hotmail.com>
2016-12-03 12:27:45 +0100

    modificacion fichero

--- scripts/ship.py
+++ scripts/ship.py
@@ -8,5 +8,13 @@ class Nave():
 
         #Cargamos la imagen de la nave y conseguimos sus recta.
         self.imagen = pygame.image.load('images/ship.bmp')
-        self.recta = self.imagen.get.rect()
-        self.pantalla_recta = pantalla.get_rect()
+        self.recta = self.imagen.get_recta()
+        self.pantalla_recta = pantalla.get_recta()
+
+        #comenzamos cada nave al fondo de la pantalla centrada
+        self.recta.centerx = self.pantalla_recta.centerx
+        self.recta.fondo = self.pantalla_recta.fondo
+
+    def blitgme(self):
+        """Dibujo la nave en su posición actual"""
+        self.pantalla.blit(self.imagen, self.recta)
